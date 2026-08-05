7bc165d573059306408c9a64d6b4c7e77f101ab0
pedro-sky <pedroassis100@gmail.com>
2026-08-05 16:58:39 -0300

    correção sintaxe

--- src/main/java/com/pedro/financeiro_api/config/CorsConfig.java
+++ src/main/java/com/pedro/financeiro_api/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.pedro.financeiro_api.config;
 
-import org.springframework.security.config.Customizer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
